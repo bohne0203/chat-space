0e5585b86a53fd7ecbcfc28fcb972b1d272ee6a8
Ko Watanabe <lsbean82@gmail.com>
2018-11-02 18:50:38 +0900

    Add Create group tool

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :update,:edit]
+  resources :groups, only: [:new, :create, :update, :edit] do
+    resources :messages, only: [:index, :create]
+  end
 end
