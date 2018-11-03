35f5cddde451e714f53ee21c73be81732ee79275
Ko Watanabe <lsbean82@gmail.com>
2018-11-03 15:17:52 +0900

    Install carrier wave

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,16 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_param)
+    if @message.save
+      redirect_to root_path
+    else
+      render :index, notice: "メッセージを入力してください"
+    end
   end
 
+  private
+
+  def message_param
+    params.require(:message).permit(:name, { :user_ids, :group_ids => [] })
 end
