c73c15532bf784e91df3da97975bc48ff50b662f
Ko Watanabe <lsbean82@gmail.com>
2018-11-04 10:23:02 +0900

    Add send-message tool

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_group
 
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
-    @message = Message.new(message_param)
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to root_path
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-      render :index, notice: "メッセージを入力してください"
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください。'
+      render :index
     end
   end
 
   private
 
-  def message_param
-    params.require(:message).permit(:name, { :user_ids, :group_ids => [] })
+  def message_params
+    params.require(:message).permit(:content,:image).merge(user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
 end
