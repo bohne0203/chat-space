a3726e4db34f9168db53e61041c09f1cfad36a54
Ko Watanabe <lsbean82@gmail.com>
2018-11-14 16:20:55 +0900

    Edit group.js for addUser function

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,4 +1,5 @@
 $(function(){
+
   var user_result = $("#user-search-result");
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
@@ -6,6 +7,16 @@ $(function(){
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
     user_result.append(html);
+  }
+
+  var user_list = $('#chat-group-user');
+  function addUser(user_id,user_name){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+                   <p class='chat-group-user__name'>${user_name}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    user_list.append(html);
   }
 
   $("#user-search-field").on("keyup", function(){
@@ -19,16 +30,29 @@ $(function(){
       })
 
       .done(function(users) {
-       $("#user-search-result").empty();
-         if (users.length !== 0) {
-           users.forEach(function(user){
+        $("#user-search-result").empty();
+        if (users.length !== 0) {
+          users.forEach(function(user){
              appendUser(user);
-           });
-         }
+          });
+        }
       })
       .fail(function(){
         alert('ユーザー検索に失敗しました')
       });
 
-   });
+    $('#user-search-result').on('click','.chat-group-user__btn--add',function(){
+      console.log(this);
+      var user_id = $(this).attr('data-user-id');
+      var user_name = $(this).attr('data-user-name');
+      addUser(user_id,user_name);
+      $(this).parent().remove();
+
+    });
   });
+
+  $(document).on('click', '.js-remove-btn', function(){
+      $(this).parent().remove();
+  });
+
+});
