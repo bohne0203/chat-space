f98cbd7b81d3bc7d8ecc53b7c6245468c0989846
Ko Watanabe <lsbean82@gmail.com>
2018-11-20 14:32:20 +0900

    Change basket name

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'uppppp-load'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/uppppp-load'
+  config.fog_directory  = 'for-upload-chatspace'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/for-upload-chatspace'
 end
